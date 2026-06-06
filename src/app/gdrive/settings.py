6da3f103273a1568b877f7d82ed53b6f28e3e5aa
João Henrique Manjabosco <manjabosco98@gmail.com>
2026-06-06 19:01:48 -0300

    Atualizando config.py

--- src/app/gdrive/settings.py
+++ src/app/gdrive/settings.py
@@ -1,14 +1,26 @@
 import os
 from pathlib import Path
 
-
 BASE_DIR = Path.cwd()
-GOOGLE_OAUTH_CREDENTIALS = os.getenv("GOOGLE_OAUTH_CREDENTIALS", "credentials/api_google_drive_auth.json")
 
-GOOGLE_OAUTH_CREDENTIALS = BASE_DIR / "credentials" / "api_google_drive_auth.json"
-GOOGLE_OAUTH_TOKEN = BASE_DIR / "credentials" / "token_drive.json"
+GOOGLE_OAUTH_CREDENTIALS = Path(
+    os.getenv(
+        "GOOGLE_OAUTH_CREDENTIALS",
+        BASE_DIR / "credentials" / "api_google_drive_auth.json"
+    )
+)
 
-GOOGLE_DRIVE_FOLDER_ID = "1w3AY5xp-hHyQh263jfvhGoUdzH-0Ls0F"
+GOOGLE_OAUTH_TOKEN = Path(
+    os.getenv(
+        "GOOGLE_OAUTH_TOKEN",
+        BASE_DIR / "credentials" / "token_drive.json"
+    )
+)
+
+GOOGLE_DRIVE_FOLDER_ID = os.getenv(
+    "GOOGLE_DRIVE_FOLDER_ID",
+    "1w3AY5xp-hHyQh263jfvhGoUdzH-0Ls0F"
+)
 
 PDF_MIME_TYPE = "application/pdf"
 XLSX_MIME_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
